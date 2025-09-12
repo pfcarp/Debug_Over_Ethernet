9450151a74e2b386cb818165ec6cc3f4d9446932
Denis Hoornaert <denis.hoornaert@tum.de>
2025-09-12 14:12:47 +0200

    top module: wrap frameformer in USW/ZCU102

--- hw/spinal/DoETH/TopModule.scala
+++ hw/spinal/DoETH/TopModule.scala
@@ -6,6 +6,7 @@ import spinal.lib._
 import spinal.lib.bus.amba4.axi.Axi4
 import spinal.lib.bus.amba4.axilite.AxiLite4
 import spinal.lib.bus.amba4.axilite.AxiLite4Utils.Axi4Rich
+import spinal.lib.bus.amba4.axis._
 
 
 import ultrascaleplus.clock.PLClockingArea
@@ -16,9 +17,10 @@ import zcu102._
 import zcu102.io.ethernet._
 
 
-case class EthernetBB() extends ZCU102(
+case class TopModule() extends ZCU102(
   config    = new ZCU102Config(
     withPL_CLK0   = 250.00 MHz,
+    withPL_CLK1   =  75.00 MHz,
     withLPD_HPM0  =       true,
     with_GT0      =       true,
     withSI570_MGT = 156.25 MHz,
@@ -27,6 +29,7 @@ case class EthernetBB() extends ZCU102(
 ) {
 
   io.lpd.hpm0.associate(io.pl.clk0)
+  io.trace.clk.associate(io.pl.clk0)
 
   val stream = Axi4Stream(Ethernet.AxiTXConfig)
   
@@ -38,25 +41,33 @@ case class EthernetBB() extends ZCU102(
     ctrl.io.tx.axis << stream
   }
 
-  val frameformer = new FrameFormerFlow(32, stream.config.dataWidth, 1)
-  frameformer.io.clockSub := // TODO: this clock domain clock
-  frameformer.io.resetSub := // TODO; this clock domain reset
-  frameformer.io.clockMan := ethernet.ctrl.io.tx.clk.o
-  frameformer.io.resetMan := ethernet.ctrl.io.user.tx.reset
-  frameformer.io.subordinate << Flow(io.trace) // If yes, who drives `valid`?
-  // OR: frameformer.io.subordinate.valid := io.trace.ctl
-  //     frameformer.io.subordinate.payload := io.trace.data
-  stream << frameformer.io.manager
+  val frameformer = new FrameFormer(
+    traceWidth        = io.trace.bus.width,
+    traceClockDomain  = io.trace.clk.domain,
+    streamConfig      = stream.config,
+    streamClockDomain = ethernet.ctrl.getClockDomain(),
+    depth             = 4
+  )
+  // Configuration
+  frameformer.io.debug.destination := B(BigInt("DEADBEEFCAFE", 16), 48 bits)
+  frameformer.io.debug.source      := B(BigInt("ABADFACEBEAD", 16), 48 bits)
+  frameformer.io.debug.linkType    := B(BigInt(        "1337", 16), 16 bits)
+  frameformer.io.debug.startWord   := B(BigInt(        "BAAB", 16), 16 bits)
+  frameformer.io.debug.endWord     := B(BigInt(        "BEEB", 16), 16 bits)
+  frameformer.io.debug.packetSize  := U(                         4, 16 bits)
+  // Connection
+  frameformer.io.subordinate.flow  << io.trace.bus.asFlow
+  frameformer.io.manager.axis  >> stream
 
   this.generate()
 }
 
 
-object EthernetBBVerilog extends App {
-  val report = Config.spinal.generateVerilog(EthernetBB())
+object TopModuleVerilog extends App {
+  val report = Config.spinal.generateVerilog(TopModule())
   report.mergeRTLSource("mergedRTL")
 }
 
-object EthernetBBVhdl extends App {
-  Config.spinal.generateVhdl(EthernetBB())
+object TopModuleVhdl extends App {
+  Config.spinal.generateVhdl(TopModule())
 }
