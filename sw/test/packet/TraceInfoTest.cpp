7a88a50fe8babf19c86246d530b9dd412f39d436
Denis Hoornaert <denis.hoornaert@tum.de>
2025-11-17 13:50:00 +0100

    sw: test: packet: trace info

---
+++ sw/test/packet/TraceInfoTest.cpp
@@ -0,0 +1,263 @@
+#include <doctest/doctest.h>
+#include <cstdint>
+#include <vector>
+
+
+#include "Packet.hpp"
+
+
+TEST_CASE("Trace info (!info, !key, !spec, !cyct): no payload after PLCTL") {
+  // Encoding
+  std::vector<uint8_t> encoding = {0b00000000};
+  // Packet under construction
+  Packet::TraceInfo obj;
+
+  // During insertion
+  for (uint8_t byte : encoding) {
+    CHECK(!obj.isDone());
+    obj.insert(byte);
+  }
+
+  // Finally ready
+  CHECK(obj.isDone());
+}
+
+TEST_CASE("Trace info (!info, !key, !spec, !cyct): no payload after PLCTL, several PLCTL steps") {
+  // Encoding
+  std::vector<uint8_t> encoding = {0b10000000, 0b10000000, 0b00000000};
+  // Packet under construction
+  Packet::TraceInfo obj;
+
+  // During insertion
+  for (uint8_t byte : encoding) {
+    CHECK(!obj.isDone());
+    obj.insert(byte);
+  }
+
+  // Finally ready
+  CHECK(obj.isDone());
+}
+
+TEST_CASE("Trace info (info, !key, !spec, !cyct)") {
+  // Encoding
+  std::vector<uint8_t> encoding = {0b00000001, 0x80, 0x84, 0x88, 0x8C, 0x10};
+  // Packet under construction
+  Packet::TraceInfo obj;
+
+  // During insertion
+  for (uint8_t byte : encoding) {
+    CHECK(!obj.isDone());
+    obj.insert(byte);
+  }
+
+  // Finally ready
+  CHECK(obj.isDone());
+}
+
+TEST_CASE("Trace info (!info, key, !spec, !cyct)") {
+  // Encoding
+  std::vector<uint8_t> encoding = {0b00000010, 0x81, 0x85, 0x89, 0x8D, 0x11};
+  // Packet under construction
+  Packet::TraceInfo obj;
+
+  // During insertion
+  for (uint8_t byte : encoding) {
+    CHECK(!obj.isDone());
+    obj.insert(byte);
+  }
+
+  // Finally ready
+  CHECK(obj.isDone());
+}
+
+TEST_CASE("Trace info (info, key, !spec, !cyct)") {
+  // Encoding
+  std::vector<uint8_t> encoding = {0b00000011, 0x80, 0x84, 0x88, 0x8C, 0x10, 0x81, 0x85, 0x89, 0x8D, 0x11};
+  // Packet under construction
+  Packet::TraceInfo obj;
+
+  // During insertion
+  for (uint8_t byte : encoding) {
+    CHECK(!obj.isDone());
+    obj.insert(byte);
+  }
+
+  // Finally ready
+  CHECK(obj.isDone());
+}
+
+TEST_CASE("Trace info (!info, !key, spec, !cyct)") {
+  // Encoding
+  std::vector<uint8_t> encoding = {0b00000100, 0x82, 0x86, 0x8A, 0x8D, 0x12};
+  // Packet under construction
+  Packet::TraceInfo obj;
+
+  // During insertion
+  for (uint8_t byte : encoding) {
+    CHECK(!obj.isDone());
+    obj.insert(byte);
+  }
+
+  // Finally ready
+  CHECK(obj.isDone());
+}
+
+TEST_CASE("Trace info (info, !key, spec, !cyct)") {
+  // Encoding
+  std::vector<uint8_t> encoding = {0b00000101, 0x80, 0x84, 0x88, 0x8C, 0x10, 0x82, 0x86, 0x8A, 0x8D, 0x12};
+  // Packet under construction
+  Packet::TraceInfo obj;
+
+  // During insertion
+  for (uint8_t byte : encoding) {
+    CHECK(!obj.isDone());
+    obj.insert(byte);
+  }
+
+  // Finally ready
+  CHECK(obj.isDone());
+}
+
+TEST_CASE("Trace info (!info, key, !spec, !cyct)") {
+  // Encoding
+  std::vector<uint8_t> encoding = {0b00000110, 0x81, 0x85, 0x89, 0x8D, 0x11, 0x82, 0x86, 0x8A, 0x8D, 0x12};
+  // Packet under construction
+  Packet::TraceInfo obj;
+
+  // During insertion
+  for (uint8_t byte : encoding) {
+    CHECK(!obj.isDone());
+    obj.insert(byte);
+  }
+
+  // Finally ready
+  CHECK(obj.isDone());
+}
+
+TEST_CASE("Trace info (info, key, spec, !cyct)") {
+  // Encoding
+  std::vector<uint8_t> encoding = {0b00000111, 0x80, 0x84, 0x88, 0x8C, 0x10, 0x81, 0x85, 0x89, 0x8D, 0x11, 0x82, 0x86, 0x8A, 0x8D, 0x12};
+  // Packet under construction
+  Packet::TraceInfo obj;
+
+  // During insertion
+  for (uint8_t byte : encoding) {
+    CHECK(!obj.isDone());
+    obj.insert(byte);
+  }
+
+  // Finally ready
+  CHECK(obj.isDone());
+}
+
+TEST_CASE("Trace info (!info, !key, !spec, cyct)") {
+  // Encoding
+  std::vector<uint8_t> encoding = {0b00001000, 0x83, 0x87, 0x8B, 0x8F, 0x13};
+  // Packet under construction
+  Packet::TraceInfo obj;
+
+  // During insertion
+  for (uint8_t byte : encoding) {
+    CHECK(!obj.isDone());
+    obj.insert(byte);
+  }
+
+  // Finally ready
+  CHECK(obj.isDone());
+}
+
+TEST_CASE("Trace info (!info, key, !spec, cyct)") {
+  // Encoding
+  std::vector<uint8_t> encoding = {0b00001010, 0x81, 0x85, 0x89, 0x8D, 0x11, 0x83, 0x87, 0x8B, 0x8F, 0x13};
+  // Packet under construction
+  Packet::TraceInfo obj;
+
+  // During insertion
+  for (uint8_t byte : encoding) {
+    CHECK(!obj.isDone());
+    obj.insert(byte);
+  }
+
+  // Finally ready
+  CHECK(obj.isDone());
+}
+
+TEST_CASE("Trace info (info, key, !spec, cyct)") {
+  // Encoding
+  std::vector<uint8_t> encoding = {0b00001011, 0x80, 0x84, 0x88, 0x8C, 0x10, 0x81, 0x85, 0x89, 0x8D, 0x11, 0x83, 0x87, 0x8B, 0x8F, 0x13};
+  // Packet under construction
+  Packet::TraceInfo obj;
+
+  // During insertion
+  for (uint8_t byte : encoding) {
+    CHECK(!obj.isDone());
+    obj.insert(byte);
+  }
+
+  // Finally ready
+  CHECK(obj.isDone());
+}
+
+TEST_CASE("Trace info (!info, !key, spec, cyct)") {
+  // Encoding
+  std::vector<uint8_t> encoding = {0b00001100, 0x82, 0x86, 0x8A, 0x8D, 0x12, 0x83, 0x87, 0x8B, 0x8F, 0x13};
+  // Packet under construction
+  Packet::TraceInfo obj;
+
+  // During insertion
+  for (uint8_t byte : encoding) {
+    CHECK(!obj.isDone());
+    obj.insert(byte);
+  }
+
+  // Finally ready
+  CHECK(obj.isDone());
+}
+
+TEST_CASE("Trace info (info, !key, spec, cyct)") {
+  // Encoding
+  std::vector<uint8_t> encoding = {0b00001101, 0x80, 0x84, 0x88, 0x8C, 0x10, 0x82, 0x86, 0x8A, 0x8D, 0x12, 0x83, 0x87, 0x8B, 0x8F, 0x13};
+  // Packet under construction
+  Packet::TraceInfo obj;
+
+  // During insertion
+  for (uint8_t byte : encoding) {
+    CHECK(!obj.isDone());
+    obj.insert(byte);
+  }
+
+  // Finally ready
+  CHECK(obj.isDone());
+}
+
+TEST_CASE("Trace info (!info, key, spec, cyct)") {
+  // Encoding
+  std::vector<uint8_t> encoding = {0b00001110, 0x81, 0x85, 0x89, 0x8D, 0x11, 0x82, 0x86, 0x8A, 0x8D, 0x12, 0x83, 0x87, 0x8B, 0x8F, 0x13};
+  // Packet under construction
+  Packet::TraceInfo obj;
+
+  // During insertion
+  for (uint8_t byte : encoding) {
+    CHECK(!obj.isDone());
+    obj.insert(byte);
+  }
+
+  // Finally ready
+  CHECK(obj.isDone());
+}
+
+TEST_CASE("Trace info (info, key, spec, cyct)") {
+  // Encoding
+  std::vector<uint8_t> encoding = {0b00001111, 0x80, 0x84, 0x88, 0x8C, 0x10, 0x81, 0x85, 0x89, 0x8D, 0x11, 0x82, 0x86, 0x8A, 0x8D, 0x12, 0x83, 0x87, 0x8B, 0x8F, 0x13};
+  // Packet under construction
+  Packet::TraceInfo obj;
+
+  // During insertion
+  for (uint8_t byte : encoding) {
+    CHECK(!obj.isDone());
+    obj.insert(byte);
+  }
+
+  // Finally ready
+  CHECK(obj.isDone());
+}
