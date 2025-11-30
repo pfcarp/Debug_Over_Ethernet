4558dd7c2803b61fd176e784e3cd3d065ce6c362
Denis Hoornaert <denis.hoornaert@tum.de>
2025-11-30 13:28:08 +0100

    sw: test: address with context

--- sw/test/packet/AddressWithContextTest.cpp
+++ sw/test/packet/AddressWithContextTest.cpp
@@ -1,5 +1,7 @@
 #include <doctest/doctest.h>
 #include <cstdint>
+#include <iostream>
+#include <format>
 #include <vector>
 
 
@@ -22,6 +24,46 @@ TEST_CASE("AddressWithContext packet: short address and no payload") {
   CHECK(obj.isDone());
   // Compare addresses, context id, and vm id
   CHECK(obj.getAddress() == 0x0000000044332222);
+  CHECK(obj.getVmID() == 0x00000000);
+  CHECK(obj.getContextID() == 0x00000000);
+}
+
+TEST_CASE("AddressWithContext packet: short address and no payload") {
+  // Encoding
+  std::vector<uint8_t> encoding = {0x28, 0x09, 0x40, 0x00, 0x0F};
+  // Packet under construction
+  Packet::AddressWithContext obj(0b10000010);
+
+  // During insertion
+  for (uint8_t byte : encoding) {
+    CHECK(!obj.isDone());
+    obj.insert(byte);
+  }
+
+  // Finally ready
+  CHECK(obj.isDone());
+  // Compare addresses, context id, and vm id
+  CHECK(obj.getAddress() == 0x00000000004012A0);
+  CHECK(obj.getVmID() == 0x00000000);
+  CHECK(obj.getContextID() == 0x00000000);
+}
+
+TEST_CASE("AddressWithContext packet: short address and no payload") {
+  // Encoding
+  std::vector<uint8_t> encoding = {0x50, 0x12, 0x40, 0x00, 0x0F};
+  // Packet under construction
+  Packet::AddressWithContext obj(0b10000011);
+
+  // During insertion
+  for (uint8_t byte : encoding) {
+    CHECK(!obj.isDone());
+    obj.insert(byte);
+  }
+
+  // Finally ready
+  CHECK(obj.isDone());
+  // Compare addresses, context id, and vm id
+  CHECK(obj.getAddress() == 0x00000000004012A0);
   CHECK(obj.getVmID() == 0x00000000);
   CHECK(obj.getContextID() == 0x00000000);
 }
@@ -220,6 +262,46 @@ TEST_CASE("AddressWithContext packet: long address and no payload") {
   CHECK(obj.isDone());
   // Compare addresses
   CHECK(obj.getAddress() == 0x8877665544332222);
+  CHECK(obj.getVmID() == 0x00000000);
+  CHECK(obj.getContextID() == 0x00000000);
+}
+
+TEST_CASE("AddressWithContext packet: long address and no payload") {
+  // Encoding
+  std::vector<uint8_t> encoding = {0x28, 0x09, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0F};
+  // Packet under construction
+  Packet::AddressWithContext obj(0b10000101);
+
+  // During insertion
+  for (uint8_t byte : encoding) {
+    CHECK(!obj.isDone());
+    obj.insert(byte);
+  }
+
+  // Finally ready
+  CHECK(obj.isDone());
+  // Compare addresses, context id, and vm id
+  CHECK(obj.getAddress() == 0x00000000004012A0);
+  CHECK(obj.getVmID() == 0x00000000);
+  CHECK(obj.getContextID() == 0x00000000);
+}
+
+TEST_CASE("AddressWithContext packet: short address and no payload") {
+  // Encoding
+  std::vector<uint8_t> encoding = {0x50, 0x12, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0F};
+  // Packet under construction
+  Packet::AddressWithContext obj(0b10000110);
+
+  // During insertion
+  for (uint8_t byte : encoding) {
+    CHECK(!obj.isDone());
+    obj.insert(byte);
+  }
+
+  // Finally ready
+  CHECK(obj.isDone());
+  // Compare addresses, context id, and vm id
+  CHECK(obj.getAddress() == 0x00000000004012A0);
   CHECK(obj.getVmID() == 0x00000000);
   CHECK(obj.getContextID() == 0x00000000);
 }
