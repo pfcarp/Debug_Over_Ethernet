0153247969bb26c472e34055eb08f76befa1dc34
Denis Hoornaert <denis.hoornaert@tum.de>
2025-12-01 18:12:27 +0100

    sw: model: event: add 'current' attribute to mark an event as being under use

--- sw/include/model/Event.hpp
+++ sw/include/model/Event.hpp
@@ -11,10 +11,13 @@
 class Event {
 
   public:
+    bool current = false;
     std::string name;
     Color color;
 
-    Event(std::string name, Color color = Color());
+    Event(std::string name, Color color = Color(), bool current = false);
     virtual bool matches(uint64_t attempt);
+    void setAsCurrent();
+    void setAsNotCurrent();
 
 };
--- sw/src/model/Event.cpp
+++ sw/src/model/Event.cpp
@@ -1,8 +1,16 @@
 #include "Event.hpp"
 
 
-Event::Event(std::string name, Color color): name(name), color(color) {}
+Event::Event(std::string name, Color color, bool current): name(name), color(color), current(current) {}
 
 bool Event::matches(uint64_t attempt) {
   return true;
+}
+
+void Event::setAsCurrent() {
+  current = true;
+}
+
+void Event::setAsNotCurrent() {
+  current = false;
 }
